2b10b43c74be95aff63f9758c57cf46fd08c4660
IronIdk <jmsfo@iscte-iul.pt>
2023-04-23 21:13:30 +0100

    Fixed the bug on Main's logger

--- src/main/java/experiment/mice/Main.java
+++ src/main/java/experiment/mice/Main.java
@@ -4,7 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-    private static final Logger logger = Logger.getLogger(ReadFromMQTTToMongoDB.class.getName());
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
         logger.log(Level.INFO, "The Main is still in development");
