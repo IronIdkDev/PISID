89c2e7f9b079a2fa69a2b0116764bfc7e1b3d3db
IronIdk <jmsfo@iscte-iul.pt>
2023-05-03 16:33:33 +0100

    Made structure changes
    * Deleted unnecessary files, auto-generated
    
    * Started developing the main to automate the process of testing the app

--- src/main/java/experiment/mice/Main.java
+++ src/main/java/experiment/mice/Main.java
@@ -1,5 +1,7 @@
 package experiment.mice;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -7,6 +9,27 @@ public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-        logger.log(Level.INFO, "The Main is still in development");
+        JFrame frame = new JFrame("Start the program");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setLayout(new BorderLayout());
+        JButton button = new JButton("Start the Program");
+        button.setForeground(Color.WHITE);
+        button.setBackground(Color.RED);
+        button.setOpaque(true);
+        button.setBorderPainted(false);
+        button.setPreferredSize(new Dimension(100, 100));
+        button.addActionListener(e -> {
+            try {
+                String[] command = {"cmd.exe", "/c", "runas", "/user:Administrator", "cmd.exe", "/c", "cd \\ && cd mosquitto && net stop mosquitto && net start mosquitto && start cmd /k mosquitto -v -c testconfig.txt"};
+                ProcessBuilder builder = new ProcessBuilder(command);
+                builder.redirectErrorStream(true);
+                builder.start();
+            } catch (Exception ex) {
+                logger.log(Level.SEVERE, "An exception occurred", ex);
+            }
+        });
+        frame.add(button, BorderLayout.CENTER);
+        frame.setVisible(true);
     }
 }
